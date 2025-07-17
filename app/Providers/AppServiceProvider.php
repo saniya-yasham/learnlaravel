dc59a02adef095c373229f0c104b3df7de3ae62e
saniya-yasham <saniya.yasham@gmail.com>
2025-07-17 11:05:08 +0530

    LAZY LOAD OFF BY DEFAULT

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Model::preventLazyLoading();
+        Model::preventLazyLoading();
+        // if APP_ENV=production = preventLazyLoading = false
     }
 }
